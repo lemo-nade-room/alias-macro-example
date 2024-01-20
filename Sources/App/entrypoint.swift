f9db32ae5e3af2318afe0527d13a2a817fc8e98a
tanaka <lemo.nade.engineer.room@gmail.com>
2024-01-21 01:34:22 +0900

    Initial Commit

--- Sources/App/entrypoint.swift
+++ Sources/App/entrypoint.swift
@@ -1,21 +1,21 @@
-import Vapor
 import Logging
+import Vapor
 
 @main
 enum Entrypoint {
-    static func main() async throws {
-        var env = try Environment.detect()
-        try LoggingSystem.bootstrap(from: &env)
-        
-        let app = Application(env)
-        defer { app.shutdown() }
-        
-        do {
-            try await configure(app)
-        } catch {
-            app.logger.report(error: error)
-            throw error
-        }
-        try await app.execute()
+  static func main() async throws {
+    var env = try Environment.detect()
+    try LoggingSystem.bootstrap(from: &env)
+
+    let app = Application(env)
+    defer { app.shutdown() }
+
+    do {
+      try await configure(app)
+    } catch {
+      app.logger.report(error: error)
+      throw error
     }
+    try await app.execute()
+  }
 }
--- Tests/AppTests/AppTests.swift
+++ Tests/AppTests/AppTests.swift
@@ -1,15 +1,18 @@
-@testable import App
 import XCTVapor
 
-final class AppTests: XCTestCase {
-    func testHelloWorld() async throws {
-        let app = Application(.testing)
-        defer { app.shutdown() }
-        try await configure(app)
+@testable import App
 
-        try app.test(.GET, "hello", afterResponse: { res in
-            XCTAssertEqual(res.status, .ok)
-            XCTAssertEqual(res.body.string, "Hello, world!")
-        })
-    }
+final class AppTests: XCTestCase {
+  func testHelloWorld() async throws {
+    let app = Application(.testing)
+    defer { app.shutdown() }
+    try await configure(app)
+
+    try app.test(
+      .GET, "hello",
+      afterResponse: { res in
+        XCTAssertEqual(res.status, .ok)
+        XCTAssertEqual(res.body.string, "Hello, world!")
+      })
+  }
 }
